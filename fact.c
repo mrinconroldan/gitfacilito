0f155094a78a54a5e3740db0bf7109d4ee51dbbe
Manuel Rincon <manrinrol@alum.us.es>
2017-10-23 17:33:25 +0200

    commit inicial

---
+++ fact.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "calcfact.h"
+
+int main(int argc, char **argv)
+{
+  int num;
+//  printf("debug: argc [%d]\n", argc);
+  
+  if (argc!=2)
+  {
+	printf("uso: %s <numero>\n", argv[0]);
+        exit(-1);
+  }
+  num=atoi(argv[1]);
+  printf ("resultado: el factorial de [%d] es [%d].\n", num, calcfact(num));
+
+}
